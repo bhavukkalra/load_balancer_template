2e02861dd89f646567ed484c59a1586c7d4d8644
Bhavuk kalra <bhavukkalra1786@gmail.com>
2023-01-13 04:06:44 +0530

    Online users functionality working

--- client/index.js
+++ client/index.js
@@ -3,14 +3,33 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const port = 4000;
+let count = 0;
 
+// If client opens this webpage
+// A connection event will run on the server
+io.on('connection', (socket) => {
+    console.log("a user connected");
+    count++;
 
-http.listen(port, () => {
-    console.log(`listening on port ${port}`);
+    // emit sends data to all clients
+    // We can watch on client using "usercnt" event
+    io.emit('usercnt', count);
+
+    // 'disconnect' event will run if client 
+    // browser closes (Tab)
+    socket.on("disconnect", () => {
+        console.log("a user disconnected");
+        count--;
+        io.emit("usercnt", count);
+    })
 })
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 
 });
+
+http.listen(port, () => {
+    console.log(`listening on port ${port}`);
+})
 
